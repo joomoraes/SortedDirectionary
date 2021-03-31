ae607a492081e1703ca2636338b2deb39609760c
João Alves <joao.alves@liberali.local>
2021-03-31 08:38:22 -0400

    ATRIBUIÇÃO DO DICTIONARY

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DirectionaryMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            cookies["user"] = "maria";
+            cookies["email"] = "maria@gmail.com";
+            cookies["phone"] = "99712234";
+            cookies["phone"] = "83737388";
+
+            Console.WriteLine(cookies["email"]);
+            Console.WriteLine(cookies["phone"]);
+
+            cookies.Remove("email");
+
+            if(cookies.ContainsKey("email"))
+            {
+                Console.WriteLine(cookies["email"]);
+            } else
+            {
+                Console.WriteLine("There is no 'email' key ");
+            }
+
+            Console.WriteLine("Size " + cookies.Count);
+
+            Console.WriteLine("ALL COOKIES: ");
+            foreach (KeyValuePair<string, string> item in cookies)
+            {
+                Console.WriteLine(item.Key + ":" + item.Value);
+            }
+            {
+
+            }
+        }
+    }
+}
